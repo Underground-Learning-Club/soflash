a94e81399a525697edc89a9a9676b035a0a66b62
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-18 02:08:51 +0100

    showing guest

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,10 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { IFlashcard } from "./dataLayer/interfaces";
 import { getFlashcards } from "./dataLayer/appModel";
+import * as config from "./config";
+import { IAppData } from "./interfaces";
 
 interface IAppContext {
 	flashcards: IFlashcard[];
 	handleToggleFlashcard: (flashcard: IFlashcard) => void;
+	appData: IAppData; 
 }
 
 interface IAppProvider {
@@ -15,6 +18,7 @@ export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 	const [flashcards, setFlashcards] = useState<IFlashcard[]>(getFlashcards());
+	const [appData, setAppdata] = useState<IAppData>({} as IAppData);
 
 	const handleToggleFlashcard = (flashcard: IFlashcard) => {
 		flashcard.isOpen = !flashcard.isOpen;
@@ -22,11 +26,26 @@ export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 		setFlashcards(_setFlashcards);
 	};
 
+	useEffect(() => {
+		let strAppData = localStorage.getItem("appData");
+		let _appData:IAppData = {} as IAppData;
+		if (strAppData === null) {
+			strAppData = JSON.stringify(config.initialAppData);
+			localStorage.setItem(
+				"appData",
+				strAppData
+			);
+		}
+		_appData = JSON.parse(strAppData);
+		setAppdata(_appData);
+	}, []);
+
 	return (
 		<AppContext.Provider
 			value={{
 				flashcards,
-				handleToggleFlashcard
+				handleToggleFlashcard,
+				appData
 			}}
 		>
 			{children}
---
+++ src/interfaces.ts
@@ -0,0 +1,3 @@
+export interface IAppData {
+	username: string;
+}
