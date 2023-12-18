67686e0168376dc9efdd9384287c32a4ce7995d4
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-18 01:46:23 +0100

    showing guest

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,13 @@ import { Nav } from "./Nav";
 export const Header = () => {
 	return (
 		<>
-			<h1 className="text-3xl mb-3 text-slate-800">Social Flashcards</h1>
-			<Nav/>
+			<div className="flex justify-between item">
+				<h1 className="text-2xl mb-3 text-slate-800">
+					Social Flashcards
+				</h1>
+				<p className="mt-1 smallcaps">Guest</p>
+			</div>
+			<Nav />
 		</>
 	);
 };
